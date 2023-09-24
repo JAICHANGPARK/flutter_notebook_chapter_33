938709f34b98a5e729702697bbd3b5257c7802a2
JAICHANGPARK <aristojeff@gmail.com>
2023-09-24 10:12:50 +0900

    Create ep1651_main.dart

---
+++ lib/ep1651_movie_streaming_app/ep1651_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+class MovieStreamingApp extends StatelessWidget {
+  const MovieStreamingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
