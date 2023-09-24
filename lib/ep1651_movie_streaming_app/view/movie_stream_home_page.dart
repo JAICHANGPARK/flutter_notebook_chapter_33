213366b4e9a8c56029eb8fc2d841aefaf8ea196a
JAICHANGPARK <aristojeff@gmail.com>
2023-09-24 10:17:38 +0900

    Update movie_stream_home_page.dart

--- lib/ep1651_movie_streaming_app/view/movie_stream_home_page.dart
+++ lib/ep1651_movie_streaming_app/view/movie_stream_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MovieStreamingHomePage extends StatefulWidget {
   const MovieStreamingHomePage({super.key});
 
@@ -12,7 +11,36 @@ class _MovieStreamingHomePageState extends State<MovieStreamingHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+        top: true,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        height: 82,
+        color: Colors.white,
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.explore_outlined),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.explore_outlined),
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.indigo,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.download_for_offline_outlined),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.person_outline),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
