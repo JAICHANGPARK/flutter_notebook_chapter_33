1a8aceb513382c18865d29cc4186cee9bee401c1
JAICHANGPARK <aristojeff@gmail.com>
2023-09-24 10:14:05 +0900

    Update movie_stream_home_page.dart

--- lib/ep1651_movie_streaming_app/view/movie_stream_home_page.dart
+++ lib/ep1651_movie_streaming_app/view/movie_stream_home_page.dart
@@ -11,6 +11,8 @@ class MovieStreamingHomePage extends StatefulWidget {
 class _MovieStreamingHomePageState extends State<MovieStreamingHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+    );
   }
 }
