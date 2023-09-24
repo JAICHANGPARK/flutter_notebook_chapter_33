a9add14ab917fc567ab79ded5d31ace69b824823
JAICHANGPARK <aristojeff@gmail.com>
2023-09-24 10:13:45 +0900

    Create movie_stream_home_page.dart

---
+++ lib/ep1651_movie_streaming_app/view/movie_stream_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MovieStreamingHomePage extends StatefulWidget {
+  const MovieStreamingHomePage({super.key});
+
+  @override
+  State<MovieStreamingHomePage> createState() => _MovieStreamingHomePageState();
+}
+
+class _MovieStreamingHomePageState extends State<MovieStreamingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
