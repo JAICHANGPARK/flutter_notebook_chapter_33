0e5d639333b7bb49b388058b65c04d38eae93da4
JAICHANGPARK <aristojeff@gmail.com>
2023-09-24 10:15:12 +0900

    Update ep1651_main.dart

--- lib/ep1651_movie_streaming_app/ep1651_main.dart
+++ lib/ep1651_movie_streaming_app/ep1651_main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_33/ep1651_movie_streaming_app/view/movie_stream_home_page.dart';
 
 void main() {
- runApp(MovieStreamingApp());
+ runApp(const MovieStreamingApp());
 }
 
 class MovieStreamingApp extends StatelessWidget {
@@ -9,6 +10,8 @@ class MovieStreamingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      home: MovieStreamingHomePage(),
+    );
   }
 }
