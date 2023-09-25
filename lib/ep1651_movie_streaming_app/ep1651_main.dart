b14b0621b3e19e7141d477ca859f24b2466a539e
Dreamwalker <aristojeff@gmail.com>
2023-09-25 18:32:32 +0900

    Update ep1651_main.dart

--- lib/ep1651_movie_streaming_app/ep1651_main.dart
+++ lib/ep1651_movie_streaming_app/ep1651_main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_33/ep1651_movie_streaming_app/view/movie_stream_home_page.dart';
 
 void main() {
- runApp(const MovieStreamingApp());
+  runApp(
+    const MovieStreamingApp(),
+  );
 }
 
 class MovieStreamingApp extends StatelessWidget {
