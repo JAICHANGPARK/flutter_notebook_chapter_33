a121749fd67f4f475070b2e02e98447418cb1f29
JAICHANGPARK <aristojeff@gmail.com>
2023-09-30 09:59:16 +0900

    Update movie_stream_detail_page.dart

--- lib/ep1651_movie_streaming_app/view/movie_stream_detail_page.dart
+++ lib/ep1651_movie_streaming_app/view/movie_stream_detail_page.dart
@@ -18,15 +18,12 @@ class MovieStreamDetailPage extends StatelessWidget {
             child: Container(
               child: SingleChildScrollView(
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(32)
-                      ),
+                      decoration: BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.circular(32)),
                       padding: EdgeInsets.all(24),
                       margin: EdgeInsets.all(24),
-
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
@@ -38,7 +35,19 @@ class MovieStreamDetailPage extends StatelessWidget {
                           Text("Action"),
                         ],
                       ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("The Plot"),
+                        Text(
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                          maxLines: 6,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
                     )
+
                   ],
                 ),
               ),
