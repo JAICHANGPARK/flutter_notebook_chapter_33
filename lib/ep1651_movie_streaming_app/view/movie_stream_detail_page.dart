2399e12426304f8e958535e39ac9ceb9418c3b43
JAICHANGPARK <aristojeff@gmail.com>
2023-09-30 09:57:41 +0900

    Update movie_stream_detail_page.dart

--- lib/ep1651_movie_streaming_app/view/movie_stream_detail_page.dart
+++ lib/ep1651_movie_streaming_app/view/movie_stream_detail_page.dart
@@ -6,7 +6,42 @@ class MovieStreamDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Column(
+        children: [
+          Expanded(
+              child: Container(
+            decoration: BoxDecoration(
+              color: Colors.orange,
+            ),
+          )),
+          Expanded(
+            child: Container(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                      ),
+                      
+                      child: Row(
+                        children: [
+                          Icon(Icons.calendar_month),
+                          Text("17 Sep 2022"),
+                          Icon(Icons.timelapse),
+                          Text("148 Minutes"),
+                          Icon(Icons.movie),
+                          Text("Action"),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
