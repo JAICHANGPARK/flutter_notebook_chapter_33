b2d512a5b34dddaa57fd3a8992912c8d02922045
JAICHANGPARK <aristojeff@gmail.com>
2023-09-30 09:54:30 +0900

    Create movie_stream_detail_page.dart

---
+++ lib/ep1651_movie_streaming_app/view/movie_stream_detail_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MovieStreamDetailPage extends StatelessWidget {
+  const MovieStreamDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
