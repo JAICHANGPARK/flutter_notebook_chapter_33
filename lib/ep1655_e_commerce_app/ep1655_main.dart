38140bb95b901fe03c6b410621e2b3d7b2864d0e
JAICHANGPARK <aristojeff@gmail.com>
2023-10-01 10:06:10 +0900

    Create ep1655_main.dart

---
+++ lib/ep1655_e_commerce_app/ep1655_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ECommerceApp());
+}
+
+class ECommerceApp extends StatelessWidget {
+  const ECommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
