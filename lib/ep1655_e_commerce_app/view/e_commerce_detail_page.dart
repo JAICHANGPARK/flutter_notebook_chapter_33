59807a8f560dc5ca99e4e1f67076651de45954b8
JAICHANGPARK <aristojeff@gmail.com>
2023-10-03 09:46:56 +0900

    Update e_commerce_detail_page.dart

---
+++ lib/ep1655_e_commerce_app/view/e_commerce_detail_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class EcommerceDetailPage extends StatefulWidget {
+  const EcommerceDetailPage({super.key});
+
+  @override
+  State<EcommerceDetailPage> createState() => _EcommerceDetailPageState();
+}
+
+class _EcommerceDetailPageState extends State<EcommerceDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
