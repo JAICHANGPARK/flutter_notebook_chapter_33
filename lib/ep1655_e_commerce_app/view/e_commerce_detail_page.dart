5872cf4ea7014493a293f5b004b49af50efd26cc
JAICHANGPARK <aristojeff@gmail.com>
2023-10-04 18:08:25 +0900

    Update e_commerce_detail_page.dart

--- lib/ep1655_e_commerce_app/view/e_commerce_detail_page.dart
+++ lib/ep1655_e_commerce_app/view/e_commerce_detail_page.dart
@@ -265,7 +265,6 @@ class _EcommerceDetailPageState extends State<EcommerceDetailPage> {
               ),
               Expanded(
                   child: SelectableText(
-                    
                       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")),
               SizedBox(
                 height: 16,
@@ -312,7 +311,13 @@ class _EcommerceDetailPageState extends State<EcommerceDetailPage> {
                             child: Container(
                               color: Colors.black,
                               child: Center(
-                                child: Text("Buy Now"),
+                                child: Text(
+                                  "Buy Now",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
                               ),
                             ),
                           )
