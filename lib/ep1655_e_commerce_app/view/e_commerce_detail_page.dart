0871e32eccac83052acf688a4d832ca749b91f39
JAICHANGPARK <aristojeff@gmail.com>
2023-10-04 18:05:17 +0900

    Update e_commerce_detail_page.dart

--- lib/ep1655_e_commerce_app/view/e_commerce_detail_page.dart
+++ lib/ep1655_e_commerce_app/view/e_commerce_detail_page.dart
@@ -263,7 +263,9 @@ class _EcommerceDetailPageState extends State<EcommerceDetailPage> {
               SizedBox(
                 height: 16,
               ),
-              Expanded(child: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")),
+              Expanded(
+                  child: Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")),
               SizedBox(
                 height: 16,
               ),
@@ -271,13 +273,31 @@ class _EcommerceDetailPageState extends State<EcommerceDetailPage> {
                 children: [
                   Expanded(
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Price"),
-                        Text("\$17.00"),
+                        Text(
+                          "Price",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          "\$17.00",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.deepOrange,
+                          ),
+                        ),
                       ],
                     ),
                   ),
-                  Expanded(child: Container())
+                  Expanded(
+                    child: Container(
+                      child: Row(
+                        children: [
+                          
+                        ],
+                      ),
+                    ),
+                  )
                 ],
               )
             ],
