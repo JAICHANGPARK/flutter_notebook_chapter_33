7b3d2c7b98f851415bdd876104d890121c913948
JAICHANGPARK <aristojeff@gmail.com>
2023-10-04 18:04:20 +0900

    Update e_commerce_detail_page.dart

--- lib/ep1655_e_commerce_app/view/e_commerce_detail_page.dart
+++ lib/ep1655_e_commerce_app/view/e_commerce_detail_page.dart
@@ -260,7 +260,13 @@ class _EcommerceDetailPageState extends State<EcommerceDetailPage> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              SizedBox(
+                height: 16,
+              ),
               Expanded(child: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")),
+              SizedBox(
+                height: 16,
+              ),
               Row(
                 children: [
                   Expanded(
