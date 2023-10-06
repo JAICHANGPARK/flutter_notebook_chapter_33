caf0d118a9190f0e8e4944747ecd5c939da42e75
JAICHANGPARK <aristojeff@gmail.com>
2023-10-06 17:08:14 +0900

    Create e_commece_notification_page.dart

---
+++ lib/ep1655_e_commerce_app/view/e_commece_notification_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EcommerceNotificationPage extends StatefulWidget {
+  const EcommerceNotificationPage({super.key});
+
+  @override
+  State<EcommerceNotificationPage> createState() => _EcommerceNotificationPageState();
+}
+
+class _EcommerceNotificationPageState extends State<EcommerceNotificationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
