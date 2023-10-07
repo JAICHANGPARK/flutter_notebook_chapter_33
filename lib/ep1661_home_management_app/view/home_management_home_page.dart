e7dd9cd53a7eb7c865164d4e8ccc3d52c5a48f2e
JAICHANGPARK <aristojeff@gmail.com>
2023-10-07 10:57:59 +0900

    Create home_management_home_page.dart

---
+++ lib/ep1661_home_management_app/view/home_management_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HomeManagementHomePage extends StatefulWidget {
+  const HomeManagementHomePage({super.key});
+
+  @override
+  State<HomeManagementHomePage> createState() => _HomeManagementHomePageState();
+}
+
+class _HomeManagementHomePageState extends State<HomeManagementHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
