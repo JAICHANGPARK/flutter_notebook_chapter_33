4539ccc446e5a237c743cc8e45dcda0030ede22a
JAICHANGPARK <aristojeff@gmail.com>
2023-10-07 10:56:10 +0900

    Create ep1661_main.dart

---
+++ lib/ep1661_home_management_app/ep1661_main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeManagementApp extends StatelessWidget {
+  const HomeManagementApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
