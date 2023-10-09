1ff9ec8051b22289e5bde91e9eeef4a177da2359
JAICHANGPARK <aristojeff@gmail.com>
2023-10-09 16:59:16 +0900

    Update home_management_profile_page.dart

--- lib/ep1661_home_management_app/view/home_management_profile_page.dart
+++ lib/ep1661_home_management_app/view/home_management_profile_page.dart
@@ -37,47 +37,79 @@ class _HomeManagementProfilePageState extends State<HomeManagementProfilePage> {
                     borderRadius: BorderRadius.circular(8),
                   ),
                 ),
-                Expanded(child: Column(children: [
-                  Text("Dream Walker"),
-                  Row(
+                Expanded(
+                  child: Column(
                     children: [
-                      Icon(
-                        Icons.location_on_outlined,
+                      Text("Dream Walker"),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.location_on_outlined,
+                          ),
+                          Text("Seoul, Republic of Korea"),
+                        ],
                       ),
-                      Text("Seoul, Republic of Korea"),
+                      Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.blueAccent,
+                              ),
+                              borderRadius: BorderRadius.circular(4),
+                            ),
+                            child: Icon(
+                              Icons.phone_outlined,
+                              color: Colors.blueAccent,
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.blueAccent,
+                              ),
+                              borderRadius: BorderRadius.circular(4),
+                            ),
+                            child: Icon(
+                              Icons.phone_outlined,
+                              color: Colors.blueAccent,
+                            ),
+                          ),
+                        ],
+                      )
                     ],
                   ),
-                  Row(
+                ),
+              ],
+            ),
+            Divider(),
+            Text("About"),
+            Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              maxLines: 3,
+            ),
+            Divider(),
+            Text("BADGES"),
+            Container(
+              child: Row(
+                children: [
+                  Column(
                     children: [
                       Container(
                         decoration: BoxDecoration(
+                          shape: BoxShape.circle,
                           border: Border.all(
                             color: Colors.blueAccent,
                           ),
-                          borderRadius: BorderRadius.circular(4),
+                          color: Colors.blue[50]
                         ),
-                        child: Icon(
-                          Icons.phone_outlined,
-                          color: Colors.blueAccent,
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.blueAccent,
-                          ),
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                        child: Icon(
-                          Icons.phone_outlined,
-                          color: Colors.blueAccent,
-                        ),
-                      ),
+                        padding: EdgeInsets.all(9),
+                        child: Icon(Icons.lock_open),
+                      )
                     ],
                   )
-                ],))
-
-              ],
+                ],
+              ),
             )
           ],
         ),
