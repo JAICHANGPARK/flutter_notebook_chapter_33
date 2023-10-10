3fef46c03b086f5e0095d2cf35841a6caca2fe80
JAICHANGPARK <aristojeff@gmail.com>
2023-10-10 23:26:12 +0900

    Update home_management_profile_page.dart

--- lib/ep1661_home_management_app/view/home_management_profile_page.dart
+++ lib/ep1661_home_management_app/view/home_management_profile_page.dart
@@ -15,154 +15,157 @@ class _HomeManagementProfilePageState extends State<HomeManagementProfilePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-
-              ],
-            ),
-            Container(
-              height: 42,
-              width: 42,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4),
-                border: Border.all(
-                  color: Colors.grey[300]!,
-                ),
-              ),
-              child: Center(
-                child: Icon(Icons.keyboard_arrow_left),
-              ),
-            ),
-            Row(
-              children: [
-                Container(
-                  height: 120,
-                  width: 120,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text("Dream Walker"),
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.location_on_outlined,
-                          ),
-                          Text("Seoul, Republic of Korea"),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.blueAccent,
-                              ),
-                              borderRadius: BorderRadius.circular(4),
-                            ),
-                            child: Icon(
-                              Icons.phone_outlined,
-                              color: Colors.blueAccent,
-                            ),
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.blueAccent,
-                              ),
-                              borderRadius: BorderRadius.circular(4),
-                            ),
-                            child: Icon(
-                              Icons.phone_outlined,
-                              color: Colors.blueAccent,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Divider(),
-            Text("About"),
-            Text(
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-              maxLines: 3,
-            ),
-            Divider(),
-            Text("BADGES"),
-            Container(
-              padding: EdgeInsets.all(7),
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey[200]!,
-                  ),
-                  borderRadius: BorderRadius.circular(4)),
-              child: Row(
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Column(
+                  Container(
+                    height: 42,
+                    width: 42,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      border: Border.all(
+                        color: Colors.grey[300]!,
+                      ),
+                    ),
+                    child: Center(
+                      child: Icon(Icons.keyboard_arrow_left),
+                    ),
+                  ),
+                  Row(
                     children: [
                       Container(
+                        height: 120,
+                        width: 120,
                         decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: Colors.blueAccent,
-                            ),
-                            color: Colors.blue[50]),
-                        padding: EdgeInsets.all(9),
-                        child: Icon(Icons.lock_open),
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
                       ),
-                      Text("Bronze"),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Text("Dream Walker"),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.location_on_outlined,
+                                ),
+                                Text("Seoul, Republic of Korea"),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.blueAccent,
+                                    ),
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  child: Icon(
+                                    Icons.phone_outlined,
+                                    color: Colors.blueAccent,
+                                  ),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.blueAccent,
+                                    ),
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  child: Icon(
+                                    Icons.phone_outlined,
+                                    color: Colors.blueAccent,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
                     ],
-                  )
+                  ),
+                  Divider(),
+                  Text("About"),
+                  Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    maxLines: 3,
+                  ),
+                  Divider(),
+                  Text("BADGES"),
+                  Container(
+                    padding: EdgeInsets.all(7),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey[200]!,
+                        ),
+                        borderRadius: BorderRadius.circular(4)),
+                    child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    color: Colors.blueAccent,
+                                  ),
+                                  color: Colors.blue[50]),
+                              padding: EdgeInsets.all(9),
+                              child: Icon(Icons.lock_open),
+                            ),
+                            Text("Bronze"),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Text("PERFORMANCE"),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey[300]!,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(9),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Text("RESPONSE RATE"),
+                              Text("90%"),
+                            ],
+                          ),
+                        ),
+                        VerticalDivider(),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Text("RESPONSE RATE"),
+                              Text("90%"),
+                            ],
+                          ),
+                        ),
+                        VerticalDivider(),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Text("RESPONSE RATE"),
+                              Text("90%"),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-            Text("PERFORMANCE"),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey[300]!,
-                ),
-              ),
-              padding: EdgeInsets.all(9),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Text("RESPONSE RATE"),
-                        Text("90%"),
-                      ],
-                    ),
-                  ),
-                  VerticalDivider(),
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Text("RESPONSE RATE"),
-                        Text("90%"),
-                      ],
-                    ),
-                  ),
-                  VerticalDivider(),
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Text("RESPONSE RATE"),
-                        Text("90%"),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
