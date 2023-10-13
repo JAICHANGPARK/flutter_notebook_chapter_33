15184f6d797fa3fe8a549048138bf62fd6235e4c
JAICHANGPARK <aristojeff@gmail.com>
2023-10-13 18:26:31 +0900

    Create ep1667_main.dart

---
+++ lib/ep1667_news_app_with_audio_podcast/ep1667_main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewsAppWithPodcast extends StatelessWidget {
+  const NewsAppWithPodcast({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
