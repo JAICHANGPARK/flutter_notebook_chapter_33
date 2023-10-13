871b1491319e33f4840f82b3b12cbc80c84eee39
JAICHANGPARK <aristojeff@gmail.com>
2023-10-13 18:33:47 +0900

    Update news_app_onboarding_page.dart

--- lib/ep1667_news_app_with_audio_podcast/views/news_app_onboarding_page.dart
+++ lib/ep1667_news_app_with_audio_podcast/views/news_app_onboarding_page.dart
@@ -42,6 +42,13 @@ class _NewsAppOnboardingPageState extends State<NewsAppOnboardingPage> {
                       fontSize: 24,
                     ),
                   ),
+                ),
+                Spacer(),
+                Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 )
               ],
             ),
