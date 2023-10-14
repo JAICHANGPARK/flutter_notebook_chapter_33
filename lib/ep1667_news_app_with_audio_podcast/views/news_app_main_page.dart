16c72f2660279d6fd2645cd2688a51e7858a1688
JAICHANGPARK <aristojeff@gmail.com>
2023-10-14 09:37:46 +0900

    Update news_app_main_page.dart

--- lib/ep1667_news_app_with_audio_podcast/views/news_app_main_page.dart
+++ lib/ep1667_news_app_with_audio_podcast/views/news_app_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class NewAppMainPage extends StatefulWidget {
   const NewAppMainPage({super.key});
 
@@ -12,13 +11,21 @@ class _NewAppMainPageState extends State<NewAppMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: SafeArea(
         child: Column(
           children: [
             Row(
               children: [
-
+                CircleAvatar(),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8),
+                      bottomRight: Radius.circular(8)
+                    )
+                  ),
+                )
               ],
             )
           ],
