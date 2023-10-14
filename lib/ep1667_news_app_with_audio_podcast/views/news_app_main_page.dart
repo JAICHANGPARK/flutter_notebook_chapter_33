0a1d84f564730165f3a62bd2e455b6daa2919170
JAICHANGPARK <aristojeff@gmail.com>
2023-10-14 09:34:53 +0900

    Create news_app_main_page.dart

---
+++ lib/ep1667_news_app_with_audio_podcast/views/news_app_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class NewAppMainPage extends StatefulWidget {
+  const NewAppMainPage({super.key});
+
+  @override
+  State<NewAppMainPage> createState() => _NewAppMainPageState();
+}
+
+class _NewAppMainPageState extends State<NewAppMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
