b51f94c3b9753d5b41e989ed57a550715f0caf00
JAICHANGPARK <aristojeff@gmail.com>
2023-10-14 09:40:49 +0900

    Update news_app_main_page.dart

--- lib/ep1667_news_app_with_audio_podcast/views/news_app_main_page.dart
+++ lib/ep1667_news_app_with_audio_podcast/views/news_app_main_page.dart
@@ -14,44 +14,47 @@ class _NewAppMainPageState extends State<NewAppMainPage> {
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-             
-              children: [
-                CircleAvatar(),
-                Expanded(
-                  child: Center(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(8),
-                          bottomRight: Radius.circular(8),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+
+                children: [
+                  CircleAvatar(),
+                  Expanded(
+                    child: Center(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(8),
+                            bottomRight: Radius.circular(8),
+                          ),
                         ),
-                      ),
-                      padding: EdgeInsets.symmetric(
-                        vertical: 4,
-                        horizontal: 12,
-                      ),
-                      child: Text(
-                        "THE NEWS",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
+                        padding: EdgeInsets.symmetric(
+                          vertical: 4,
+                          horizontal: 12,
+                        ),
+                        child: Text(
+                          "THE NEWS",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.search),
-                ),
-                Badge(
-                  child: Icon(
-                    Icons.notifications,
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.search),
                   ),
-                )
-              ],
+                  Badge(
+                    child: Icon(
+                      Icons.notifications,
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
