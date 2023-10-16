32e4c125b6511062317e754ca6c35c94b9d86d61
JAICHANGPARK <aristojeff@gmail.com>
2023-10-16 17:54:24 +0900

    Update news_app_audio_page.dart

--- lib/ep1667_news_app_with_audio_podcast/views/news_app_audio_page.dart
+++ lib/ep1667_news_app_with_audio_podcast/views/news_app_audio_page.dart
@@ -23,6 +23,43 @@ class _NewsAppAudioPageState extends State<NewsAppAudioPage> {
         title: Text("Politics"),
         centerTitle: true,
       ),
+      body: Column(
+        children: [
+          Container(
+            height: 320,
+            decoration: BoxDecoration(
+              color: Colors.pink,
+            ),
+          ),
+          Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+          Text("Lorem ipsum"),
+          Container(
+            height: 64,
+            color: Colors.purple,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("2:20"),
+              Text("10:00"),
+            ],
+          ),
+          Row(
+            children: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.shuffle)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.skip_previous)),
+              CircleAvatar(
+                radius: 28,
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                child: Icon(Icons.pause),
+              ),
+              IconButton(onPressed: () {}, icon: Icon(Icons.skip_next)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.list)),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
