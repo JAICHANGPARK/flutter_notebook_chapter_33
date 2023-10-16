a261120396298e70de4e24b763bfb2f9a2396017
JAICHANGPARK <aristojeff@gmail.com>
2023-10-16 17:49:29 +0900

    Create news_app_audio_page.dart

---
+++ lib/ep1667_news_app_with_audio_podcast/views/news_app_audio_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewsAppAudioPage extends StatefulWidget {
+  const NewsAppAudioPage({super.key});
+
+  @override
+  State<NewsAppAudioPage> createState() => _NewsAppAudioPageState();
+}
+
+class _NewsAppAudioPageState extends State<NewsAppAudioPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
