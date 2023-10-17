1da52c3cda73e76f56eb49db07de5175b8a0f7ea
JAICHANGPARK <aristojeff@gmail.com>
2023-10-17 18:32:14 +0900

    Update ecommerce_main_page.dart

--- lib/ep1671_ecommerce_app/view/ecommerce_main_page.dart
+++ lib/ep1671_ecommerce_app/view/ecommerce_main_page.dart
@@ -11,6 +11,7 @@ class _EcommerceMainPageState extends State<EcommerceMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       body: Column(
         children: [
           Container(
