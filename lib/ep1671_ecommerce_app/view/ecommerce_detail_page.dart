a7e7b8d9c70d2161b3751160a02a2dc6eddf17cb
JAICHANGPARK <aristojeff@gmail.com>
2023-10-20 20:40:08 +0900

    Update ecommerce_detail_page.dart

--- lib/ep1671_ecommerce_app/view/ecommerce_detail_page.dart
+++ lib/ep1671_ecommerce_app/view/ecommerce_detail_page.dart
@@ -11,6 +11,6 @@ class EcommerceDetailPage extends StatefulWidget {
 class _EcommerceDetailPageState extends State<EcommerceDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
