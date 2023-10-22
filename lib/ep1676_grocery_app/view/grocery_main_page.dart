4fdfa6d2a58ebbd191d67141791415fe61172103
JAICHANGPARK <aristojeff@gmail.com>
2023-10-22 10:52:30 +0900

    Update grocery_main_page.dart

--- lib/ep1676_grocery_app/view/grocery_main_page.dart
+++ lib/ep1676_grocery_app/view/grocery_main_page.dart
@@ -10,6 +10,23 @@ class GroceryMainPage extends StatefulWidget {
 class _GroceryMainPageState extends State<GroceryMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            child: Container(
+              child: Row(
+                children: [
+                  IconButton(onPressed: (){}, icon: Icon(Icons.home_filled),),
+                  IconButton(onPressed: (){}, icon: Icon(Icons.apps),),
+                  IconButton(onPressed: (){}, icon: Icon(Icons.folder),),
+                  IconButton(onPressed: (){}, icon: Icon(Icons.person_outline),),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
