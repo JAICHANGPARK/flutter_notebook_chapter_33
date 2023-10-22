30a4c6d5e3907ffe8129ae41d82190b6ae4ea1a2
JAICHANGPARK <aristojeff@gmail.com>
2023-10-22 10:50:52 +0900

    Create grocery_main_page.dart

---
+++ lib/ep1676_grocery_app/view/grocery_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GroceryMainPage extends StatefulWidget {
+  const GroceryMainPage({super.key});
+
+  @override
+  State<GroceryMainPage> createState() => _GroceryMainPageState();
+}
+
+class _GroceryMainPageState extends State<GroceryMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
