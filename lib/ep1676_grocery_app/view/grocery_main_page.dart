b26f72e4c4f1c3beab9b1b4ea6981bcce75b0a2e
JAICHANGPARK <aristojeff@gmail.com>
2023-10-22 11:00:56 +0900

    Update grocery_main_page.dart

--- lib/ep1676_grocery_app/view/grocery_main_page.dart
+++ lib/ep1676_grocery_app/view/grocery_main_page.dart
@@ -14,87 +14,95 @@ class _GroceryMainPageState extends State<GroceryMainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(244, 246, 246, 1),
-      body: Stack(
-        children: [
-          Positioned(
-            left: 24,
-            right: 24,
-            bottom: 24,
-            child: Container(
-              height: 72,
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(32),
-                boxShadow: [
-                  const BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 8,
-                    spreadRadius: 2,
-                  ),
-                ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  menuIndex == 0
-                      ? Container(
-                          decoration: BoxDecoration(
-                            color: Colors.green[50],
-                            borderRadius: BorderRadius.circular(32)
-                          ),
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 32,
-                            vertical: 16,
-                          ),
-                          child: Center(
-                            child: Text(
-                              "HOME",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w900,
-                                fontSize: 18,
-                                color: Colors.green,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              top: 24,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Column(
+              children: [Card()],
+            )),
+            Positioned(
+              left: 24,
+              right: 24,
+              bottom: 24,
+              child: Container(
+                height: 72,
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(32),
+                  boxShadow: [
+                    const BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 8,
+                      spreadRadius: 2,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    menuIndex == 0
+                        ? Container(
+                            decoration: BoxDecoration(color: Colors.green[50], borderRadius: BorderRadius.circular(32)),
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 32,
+                              vertical: 16,
+                            ),
+                            child: Center(
+                              child: Text(
+                                "HOME",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 18,
+                                  color: Colors.green,
+                                ),
                               ),
                             ),
+                          )
+                        : IconButton(
+                            onPressed: () {
+                              setState(() {
+                                menuIndex = 0;
+                              });
+                            },
+                            icon: const Icon(Icons.home_filled),
                           ),
-                        )
-                      : IconButton(
-                          onPressed: () {
-                            setState(() {
-                              menuIndex = 0;
-                            });
-                          },
-                          icon: const Icon(Icons.home_filled),
-                        ),
-                  IconButton(
-                    onPressed: () {
-                      setState(() {
-                        menuIndex = 1;
-                      });
-                    },
-                    icon: const Icon(Icons.apps),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      setState(() {
-                        menuIndex = 2;
-                      });
-                    },
-                    icon: const Icon(Icons.folder),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      setState(() {
-                        menuIndex = 3;
-                      });
-                    },
-                    icon: const Icon(Icons.person_outline),
-                  ),
-                ],
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          menuIndex = 1;
+                        });
+                      },
+                      icon: const Icon(Icons.apps),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          menuIndex = 2;
+                        });
+                      },
+                      icon: const Icon(Icons.folder),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          menuIndex = 3;
+                        });
+                      },
+                      icon: const Icon(Icons.person_outline),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
