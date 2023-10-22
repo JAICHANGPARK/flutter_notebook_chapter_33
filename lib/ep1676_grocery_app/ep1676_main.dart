a6a5ba3ac98271d8b396d04ce5398acc2e9d3c2d
JAICHANGPARK <aristojeff@gmail.com>
2023-10-22 10:49:02 +0900

    Update ep1676_main.dart

---
+++ lib/ep1676_grocery_app/ep1676_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GroceryApp());
+}
+
+class GroceryApp extends StatelessWidget {
+  const GroceryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
