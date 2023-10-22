821e7a478a85785c6daa16f594738c8ae63ab210
JAICHANGPARK <aristojeff@gmail.com>
2023-10-22 11:15:17 +0900

    Update grocery_main_page.dart

--- lib/ep1676_grocery_app/view/grocery_main_page.dart
+++ lib/ep1676_grocery_app/view/grocery_main_page.dart
@@ -18,81 +18,112 @@ class _GroceryMainPageState extends State<GroceryMainPage> {
         child: Stack(
           children: [
             Positioned(
-                top: 24,
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8),
-                      child: Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(2.0),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "2464 Royal Ln, Mesa",
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 8,
-                                      ),
-                                      Text(
-                                        "Your address",
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.grey,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.green[50],
-                                  borderRadius: BorderRadius.circular(32),
-                                ),
-                                padding: EdgeInsets.symmetric(
-                                  horizontal: 16,
-                                  vertical: 16,
-                                ),
-                                child: Row(
+              top: 24,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Row(
+                          children: [
+                            const Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    Icon(
-                                      Icons.shopping_bag_outlined,
-                                      color: Colors.green,
-                                    ),
-                                    SizedBox(
-                                      width: 8,
-                                    ),
                                     Text(
-                                      "02",
+                                      "2464 Royal Ln, Mesa",
                                       style: TextStyle(
-                                        color: Colors.green,
-                                        fontSize: 24,
                                         fontWeight: FontWeight.bold,
                                       ),
-                                    )
+                                    ),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Text(
+                                      "Your address",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
                                   ],
                                 ),
-                              )
-                            ],
+                              ),
+                            ),
+                            Container(
+                              decoration:
+                                  BoxDecoration(color: Colors.green[50], borderRadius: BorderRadius.circular(32)),
+                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                              child: const Row(
+                                children: [
+                                  Icon(
+                                    Icons.shopping_bag_outlined,
+                                    color: Colors.green,
+                                  ),
+                                  SizedBox(
+                                    width: 8,
+                                  ),
+                                  Text(
+                                    "02",
+                                    style: TextStyle(
+                                      color: Colors.green,
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Search",
+                            suffixIcon: Icon(Icons.search_outlined),
+                            border: InputBorder.none,
                           ),
                         ),
                       ),
                     ),
-                  ],
-                )),
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text("Categories"),
+                              TextButton(
+                                onPressed: () {},
+                                child: const Text("View All"),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
             Positioned(
               left: 24,
               right: 24,
