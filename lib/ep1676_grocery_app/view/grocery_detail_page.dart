b01e69fd368d342f6854e87dcb476a044af401ce
JAICHANGPARK <aristojeff@gmail.com>
2023-10-25 18:34:14 +0900

    Update grocery_detail_page.dart

---
+++ lib/ep1676_grocery_app/view/grocery_detail_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class GroceryDetailPage extends StatefulWidget {
+  const GroceryDetailPage({super.key});
+
+  @override
+  State<GroceryDetailPage> createState() => _GroceryDetailPageState();
+}
+
+class _GroceryDetailPageState extends State<GroceryDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
