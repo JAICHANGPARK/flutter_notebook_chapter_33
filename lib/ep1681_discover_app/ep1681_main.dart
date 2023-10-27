10762182b048a54bfb43009ed6f1ea11539074f8
JAICHANGPARK <aristojeff@gmail.com>
2023-10-27 17:15:50 +0900

    Update ep1681_main.dart

--- lib/ep1681_discover_app/ep1681_main.dart
+++ lib/ep1681_discover_app/ep1681_main.dart
@@ -7,6 +7,8 @@ class DiscoverMobileApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      
+    );
   }
 }
