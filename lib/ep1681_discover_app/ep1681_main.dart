f6eca566ed6dd90370736ce331f2053dfda35980
JAICHANGPARK <aristojeff@gmail.com>
2023-10-27 17:14:40 +0900

    Create ep1681_main.dart

---
+++ lib/ep1681_discover_app/ep1681_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class DiscoverMobileApp extends StatelessWidget {
+  const DiscoverMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
