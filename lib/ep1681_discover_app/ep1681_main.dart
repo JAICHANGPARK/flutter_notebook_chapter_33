a6d2a58330db49793aec84a10e161c1fe5b9ad19
JAICHANGPARK <aristojeff@gmail.com>
2023-10-27 17:17:49 +0900

    Update ep1681_main.dart

--- lib/ep1681_discover_app/ep1681_main.dart
+++ lib/ep1681_discover_app/ep1681_main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_33/ep1681_discover_app/view/discover_mobile_main_page.dart';
 
-void main() => runApp(DiscoverMobileApp());
+void main() => runApp(const DiscoverMobileApp());
 
 class DiscoverMobileApp extends StatelessWidget {
   const DiscoverMobileApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ,
+    return const MaterialApp(
+      home: DiscoverMobileMainPage(),
     );
   }
 }
