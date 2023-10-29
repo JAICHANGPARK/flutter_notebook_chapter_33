c251169f219c7de32a2330911f0c06457181ecaa
JAICHANGPARK <aristojeff@gmail.com>
2023-10-29 09:56:00 +0900

    Create discover_menu_page.dart

---
+++ lib/ep1681_discover_app/view/discover_menu_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DiscoverMenuPage extends StatefulWidget {
+  const DiscoverMenuPage({super.key});
+
+  @override
+  State<DiscoverMenuPage> createState() => _DiscoverMenuPageState();
+}
+
+class _DiscoverMenuPageState extends State<DiscoverMenuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
