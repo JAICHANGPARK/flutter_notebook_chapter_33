6ac63a020a91145d2aea60b4a72d072286d9d219
JAICHANGPARK <aristojeff@gmail.com>
2023-10-29 10:02:25 +0900

    Update discover_menu_page.dart

--- lib/ep1681_discover_app/view/discover_menu_page.dart
+++ lib/ep1681_discover_app/view/discover_menu_page.dart
@@ -26,13 +26,48 @@ class _DiscoverMenuPageState extends State<DiscoverMenuPage> {
         ],
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             height: 400,
+            margin: EdgeInsets.all(16),
             color: Colors.green,
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 0,
+                  left: 32,
+                  right: 32,
+                  bottom: 0,
+                  child: Card(
+                    elevation: 8,
+                    child: Center(),
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  left: 16,
+                  right: 16,
+                  bottom: 16,
+                  child: Card(
+                    elevation: 8,
+                    child: Center(),
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 32,
+                  child: Card(
+                    elevation: 8,
+                    child: Center(),
+                  ),
+                )
+              ],
+            ),
           ),
           Text("CATEGORIES"),
-          
         ],
       ),
     );
