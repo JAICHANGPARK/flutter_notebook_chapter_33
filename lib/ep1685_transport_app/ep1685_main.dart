f8a705f0c8b0ecfb64af77a8b0300d8caf135d11
JAICHANGPARK <aristojeff@gmail.com>
2023-10-31 18:20:14 +0900

    Create ep1685_main.dart

---
+++ lib/ep1685_transport_app/ep1685_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TransportApp());
+}
+
+class TransportApp extends StatelessWidget {
+  const TransportApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
