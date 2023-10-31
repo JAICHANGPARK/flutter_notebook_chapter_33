f9cfd10952a052f9a1ee765e598123c19619e9a4
JAICHANGPARK <aristojeff@gmail.com>
2023-10-31 18:25:32 +0900

    Update transport_start_page.dart

--- lib/ep1685_transport_app/views/transport_start_page.dart
+++ lib/ep1685_transport_app/views/transport_start_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TransportStartPage extends StatefulWidget {
   const TransportStartPage({super.key});
 
@@ -11,6 +10,27 @@ class TransportStartPage extends StatefulWidget {
 class _TransportStartPageState extends State<TransportStartPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[400],
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  child: Text("Skip",style: TextStyle(
+                    
+                  ),),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
