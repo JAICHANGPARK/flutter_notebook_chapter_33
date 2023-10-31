8beb2538a4672ec3d0e467299eeb45fcc0b2c2dc
JAICHANGPARK <aristojeff@gmail.com>
2023-10-31 18:23:55 +0900

    Update transport_start_page.dart

---
+++ lib/ep1685_transport_app/views/transport_start_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TransportStartPage extends StatefulWidget {
+  const TransportStartPage({super.key});
+
+  @override
+  State<TransportStartPage> createState() => _TransportStartPageState();
+}
+
+class _TransportStartPageState extends State<TransportStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
