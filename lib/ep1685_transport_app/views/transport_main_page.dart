0aa81d8ee68f4560a9f491e7a22de84528e3356f
JAICHANGPARK <aristojeff@gmail.com>
2023-11-01 18:32:18 +0900

    Create transport_main_page.dart

---
+++ lib/ep1685_transport_app/views/transport_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TransportMainPage extends StatefulWidget {
+  const TransportMainPage({super.key});
+
+  @override
+  State<TransportMainPage> createState() => _TransportMainPageState();
+}
+
+class _TransportMainPageState extends State<TransportMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
