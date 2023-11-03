97488f0e5a12408605012af62883aac2904e2471
JAICHANGPARK <aristojeff@gmail.com>
2023-11-03 18:29:37 +0900

    Update community_main_page.dart

--- lib/ep1688_community_app/view/community_main_page.dart
+++ lib/ep1688_community_app/view/community_main_page.dart
@@ -10,6 +10,8 @@ class CommunityMainPage extends StatefulWidget {
 class _CommunityMainPageState extends State<CommunityMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
