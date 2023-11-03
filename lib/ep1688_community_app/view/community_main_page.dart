64655a666df25faebe72635c9215152c931c7d0a
JAICHANGPARK <aristojeff@gmail.com>
2023-11-03 18:28:32 +0900

    Create community_main_page.dart

---
+++ lib/ep1688_community_app/view/community_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CommunityMainPage extends StatefulWidget {
+  const CommunityMainPage({super.key});
+
+  @override
+  State<CommunityMainPage> createState() => _CommunityMainPageState();
+}
+
+class _CommunityMainPageState extends State<CommunityMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
