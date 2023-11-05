fbe20985e9fc5a50f748bbd7a0dacaaa08cc9587
JAICHANGPARK <aristojeff@gmail.com>
2023-11-05 10:15:59 +0900

    Update community_main_page.dart

--- lib/ep1688_community_app/view/community_main_page.dart
+++ lib/ep1688_community_app/view/community_main_page.dart
@@ -55,93 +55,94 @@ class _CommunityMainPageState extends State<CommunityMainPage> {
                 Column(
                   children: [
                     Expanded(
-                        child: ListView(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              CircleAvatar(
-                                radius: 12,
-                              ),
-                              SizedBox(
-                                width: 8,
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Text(
-                                          "Dreamwalker",
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                        Gap(2),
-                                        Icon(
-                                          Icons.verified,
-                                          color: Colors.blue,
-                                        ),
-                                        Gap(2),
-                                        Text(
-                                          "highlights message in chat",
-                                          style: TextStyle(
-                                            color: Colors.grey,
-                                          ),
-                                        ),
-                                        Spacer(),
-                                        Icon(
-                                          Icons.more_horiz,
-                                          color: Colors.white,
-                                        )
-                                      ],
-                                    ),
-                                    Gap(4),
-                                    Row(
-                                      children: [
-                                        Text(
-                                          "Flutter",
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                        Text(
-                                          "3h ago",
-                                          style: TextStyle(
-                                            color: Colors.grey,
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                    Gap(16),
-                                    Container(
-                                      padding: EdgeInsets.all(12),
-                                      decoration: BoxDecoration(
-                                        color: Color.fromRGBO(44, 47, 52, 1),
-                                        borderRadius: BorderRadius.circular(12),
-                                      ),
-                                      child: Text(
-                                        "사람들의 내 내 봅니다. 까닭이요, 벌레는 나는 듯합니다. 아무 우는 사람들의 잠, 다 별이 이름을 까닭입니다. 소녀들의 새겨지는 않은 하늘에는 버리었습니다. 이름과, 하나의 벌써 토끼, 새겨지는 별이 그리고 것은 없이 있습니다",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ),
-                                    
-                                    Container(
-                                      decoration: BoxDecoration(),
-                                      child: Text("OPEN CHAT"),
-                                    )
-                                  ],
+                      child: ListView(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                CircleAvatar(
+                                  radius: 12,
                                 ),
-                              ),
-                            ],
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Text(
+                                            "Dreamwalker",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                          Gap(2),
+                                          Icon(
+                                            Icons.verified,
+                                            color: Colors.blue,
+                                          ),
+                                          Gap(2),
+                                          Text(
+                                            "highlights message in chat",
+                                            style: TextStyle(
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                          Spacer(),
+                                          Icon(
+                                            Icons.more_horiz,
+                                            color: Colors.white,
+                                          )
+                                        ],
+                                      ),
+                                      Gap(4),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            "Flutter",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                          Text(
+                                            "3h ago",
+                                            style: TextStyle(
+                                              color: Colors.grey,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      Gap(16),
+                                      Container(
+                                        padding: EdgeInsets.all(12),
+                                        decoration: BoxDecoration(
+                                          color: Color.fromRGBO(44, 47, 52, 1),
+                                          borderRadius: BorderRadius.circular(12),
+                                        ),
+                                        child: Text(
+                                          "사람들의 내 내 봅니다. 까닭이요, 벌레는 나는 듯합니다. 아무 우는 사람들의 잠, 다 별이 이름을 까닭입니다. 소녀들의 새겨지는 않은 하늘에는 버리었습니다. 이름과, 하나의 벌써 토끼, 새겨지는 별이 그리고 것은 없이 있습니다",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        decoration: BoxDecoration(),
+                                        child: Text("OPEN CHAT"),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        )
-                      ],
-                    )),
+                          Divider(),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
                 Container(),
