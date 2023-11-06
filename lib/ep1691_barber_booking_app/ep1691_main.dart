fca18f48fab4da0266feab987bd7f55c47897a28
JAICHANGPARK <aristojeff@gmail.com>
2023-11-06 17:26:45 +0900

    Create ep1691_main.dart

---
+++ lib/ep1691_barber_booking_app/ep1691_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BarberBookingApp());
+}
+
+class BarberBookingApp extends StatelessWidget {
+  const BarberBookingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
