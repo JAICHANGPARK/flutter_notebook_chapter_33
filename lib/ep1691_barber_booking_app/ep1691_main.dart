1c871b35144a6f81310308f15af645d3daa0f8a1
JAICHANGPARK <aristojeff@gmail.com>
2023-11-06 17:29:21 +0900

    Update ep1691_main.dart

--- lib/ep1691_barber_booking_app/ep1691_main.dart
+++ lib/ep1691_barber_booking_app/ep1691_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_33/ep1691_barber_booking_app/views/barber_booking_main_page.dart';
 
 void main() {
   runApp(BarberBookingApp());
@@ -9,6 +10,8 @@ class BarberBookingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: BarberBookingMainPage(),
+    );
   }
 }
