f67e2100fbceac045ddea93f545c9866540a64a7
JAICHANGPARK <aristojeff@gmail.com>
2023-11-11 10:45:18 +0900

    Update consumer_home_page.dart

--- lib/ep1696_consumer_app/views/consumer_home_page.dart
+++ lib/ep1696_consumer_app/views/consumer_home_page.dart
@@ -11,22 +11,36 @@ class _ConsumerHomeAppState extends State<ConsumerHomeApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              CircleAvatar(
-                radius: 20,
-              ),
-              Column(
-                children: [
-                  Text("Dream Walker"),
-                  Text("@Dreamwalker"),
-                ],
-              )
-            ],
-          )
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 20,
+                ),
+                Column(
+                  children: [
+                    Text("Dream Walker"),
+                    Text("@Dreamwalker"),
+                  ],
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.pink[50],
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.add,
+                    ),
+                    color: Colors.pink,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
