eb9f6cad77a88b19c33887f9a1ba951a3fe292dc
JAICHANGPARK <aristojeff@gmail.com>
2023-11-11 10:43:52 +0900

    Update consumer_home_page.dart

--- lib/ep1696_consumer_app/views/consumer_home_page.dart
+++ lib/ep1696_consumer_app/views/consumer_home_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class ConsumerHomeApp extends StatefulWidget {
   const ConsumerHomeApp({super.key});
 
@@ -12,6 +10,24 @@ class ConsumerHomeApp extends StatefulWidget {
 class _ConsumerHomeAppState extends State<ConsumerHomeApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                radius: 20,
+              ),
+              Column(
+                children: [
+                  Text("Dream Walker"),
+                  Text("@Dreamwalker"),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
