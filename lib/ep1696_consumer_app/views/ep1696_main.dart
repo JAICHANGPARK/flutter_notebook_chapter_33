f2b32122e73a33c551f2605354c3dd20d111a001
JAICHANGPARK <aristojeff@gmail.com>
2023-11-11 10:39:38 +0900

    Create ep1696_main.dart

---
+++ lib/ep1696_consumer_app/views/ep1696_main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class ConsumerApp extends StatelessWidget {
+  const ConsumerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
