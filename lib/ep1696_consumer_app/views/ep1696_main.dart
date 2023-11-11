79ea74cc1e2bbbfd6126ea6ec4dd6583c3aa3d35
JAICHANGPARK <aristojeff@gmail.com>
2023-11-11 10:41:52 +0900

    Update ep1696_main.dart

--- lib/ep1696_consumer_app/views/ep1696_main.dart
+++ lib/ep1696_consumer_app/views/ep1696_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_33/ep1696_consumer_app/views/consumer_home_page.dart';
 
 void main() {
   runApp(ConsumerApp());
@@ -9,6 +10,8 @@ class ConsumerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: ConsumerHomeApp(),
+    );
   }
 }
