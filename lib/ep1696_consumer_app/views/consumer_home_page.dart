16dfb4cbb491809607ff8018c795445eeca56eb9
JAICHANGPARK <aristojeff@gmail.com>
2023-11-11 10:41:45 +0900

    Create consumer_home_page.dart

---
+++ lib/ep1696_consumer_app/views/consumer_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class ConsumerHomeApp extends StatefulWidget {
+  const ConsumerHomeApp({super.key});
+
+  @override
+  State<ConsumerHomeApp> createState() => _ConsumerHomeAppState();
+}
+
+class _ConsumerHomeAppState extends State<ConsumerHomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
