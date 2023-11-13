97e522125090d02726e06226591e310db40bc12c
JAICHANGPARK <aristojeff@gmail.com>
2023-11-13 17:15:19 +0900

    Update consumer_login_page.dart

--- lib/ep1696_consumer_app/views/consumer_login_page.dart
+++ lib/ep1696_consumer_app/views/consumer_login_page.dart
@@ -1,5 +1,5 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
-
 
 class ConsumerLoginPage extends StatefulWidget {
   const ConsumerLoginPage({super.key});
@@ -18,14 +18,28 @@ class _ConsumerLoginPageState extends State<ConsumerLoginPage> {
           padding: const EdgeInsets.all(24.0),
           child: Column(
             children: [
-                  Container(
-                    height: 200,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(16)
-                    ),
-                  )
-
+              Container(
+                height: 360,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+              Text(
+                "Discounts",
+              ),
+              Text(
+                "Get notified every time there's a discount on your saved items",
+              ),
+              DotsIndicator(
+                dotsCount: 3,
+                decorator: DotsDecorator(
+                  size: Size(48, 6),
+                  activeSize: Size(48, 6),
+                  shape: RoundedRectangleBorder(),
+                  activeShape: RoundedRectangleBorder(),
+                ),
+              ),
             ],
           ),
         ),
