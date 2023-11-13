76516cc41cd81ac308974922bef112d537451ecc
JAICHANGPARK <aristojeff@gmail.com>
2023-11-13 17:08:34 +0900

    Create consumer_login_page.dart

---
+++ lib/ep1696_consumer_app/views/consumer_login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ConsumerLoginPage extends StatefulWidget {
+  const ConsumerLoginPage({super.key});
+
+  @override
+  State<ConsumerLoginPage> createState() => _ConsumerLoginPageState();
+}
+
+class _ConsumerLoginPageState extends State<ConsumerLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
