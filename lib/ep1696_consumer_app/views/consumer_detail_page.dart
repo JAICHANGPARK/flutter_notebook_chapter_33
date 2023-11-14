73a9c3cdf50062e33cbd7601e3c28d875ce41753
JAICHANGPARK <aristojeff@gmail.com>
2023-11-14 18:50:17 +0900

    Update consumer_detail_page.dart

--- lib/ep1696_consumer_app/views/consumer_detail_page.dart
+++ lib/ep1696_consumer_app/views/consumer_detail_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class ConsumerDetailPage extends StatefulWidget {
+  const ConsumerDetailPage({super.key});
+
+  @override
+  State<ConsumerDetailPage> createState() => _ConsumerDetailPageState();
+}
+
+class _ConsumerDetailPageState extends State<ConsumerDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
