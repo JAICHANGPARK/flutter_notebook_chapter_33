71f8912bc64ea16355cdf97d6fcba9b07064ecf4
JAICHANGPARK <aristojeff@gmail.com>
2023-11-14 18:59:26 +0900

    Update consumer_detail_page.dart

--- lib/ep1696_consumer_app/views/consumer_detail_page.dart
+++ lib/ep1696_consumer_app/views/consumer_detail_page.dart
@@ -18,49 +18,66 @@ class _ConsumerDetailPageState extends State<ConsumerDetailPage> {
         title: Text("Fantasy"),
         elevation: 0,
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              Row(
-                children: [
-                  CircleAvatar(),
-                  Text("@Dreamwalker"),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.black26,
-                    ),
-                    child: Text(
-                      "Purchased",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.more_horiz,
-                    ),
-                  )
-                ],
-              ),
-              Card(
-                child: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                Row(
                   children: [
+                    CircleAvatar(),
+                    Text("@Dreamwalker"),
                     Container(
-                      height: 300,
                       decoration: BoxDecoration(
-                        color: Colors.blue,
+                        color: Colors.black26,
                       ),
-                    )
+                      child: Text(
+                        "Purchased",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.more_horiz,
+                      ),
+                    ),
                   ],
                 ),
-              ),
-            ],
-          );
-        },
+                Card(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 300,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text("AUDIBLE"),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.blueGrey,
+
+                            ),
+                            child: Text("Updated"),
+                          ),
+                          Text("\$12.98"),
+                        ],
+                      ),
+                      Text("The Flutter Dev")
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
