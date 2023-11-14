6556b924d09b171dea66fb5a56d32f55abef63dd
JAICHANGPARK <aristojeff@gmail.com>
2023-11-14 18:55:17 +0900

    Update consumer_detail_page.dart

--- lib/ep1696_consumer_app/views/consumer_detail_page.dart
+++ lib/ep1696_consumer_app/views/consumer_detail_page.dart
@@ -16,6 +16,32 @@ class _ConsumerDetailPageState extends State<ConsumerDetailPage> {
         foregroundColor: Colors.black,
         centerTitle: true,
         title: Text("Fantasy"),
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(),
+                  Text("@Dreamwalker"),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black26,
+                    ),
+                    child: Text(
+                      "Purchased",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          );
+        },
       ),
     );
   }
